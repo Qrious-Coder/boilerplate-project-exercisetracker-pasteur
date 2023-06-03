331e1ce87acc913e3902788df39bb63c935054d3
Q <q81188118@gmail.com>
2023-06-03 18:58:27 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-//import parser
+
+//Connect to mongodb
+const { MongoClient } = require("mongodb");
+const database = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+database.connect();
+
 const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
@@ -12,34 +21,75 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/users", function (req, res) {
-  const username = req.body;
+app.post("/api/users", async function (req, res) {
+  const username = req.body.username;
 
   if (!username) {
     return res.status(400).send({ error: "invalid user" });
   } else {
-    return res.json({
+    let newUser = {
       username: username,
-    });
+    };
+
+    const result = await database
+      .db("exercise_tracker")
+      .collection("users")
+      .insertOne(newUser);
+
+    newUser._id = result.insertedId;
+    //get the id
+    return res.json(newUser);
   }
 });
 
-app.post("/api/users/:_id/exercise", function (req, res) {
-  const id = req.params._id;
-  const description = req.params.description;
-  const duration = req.params.duration;
-  const date = req.params.date;
-
-  if (!id) {
-    return res.status(400).send({ error: "invalid exercise input" });
-  } else {
-    return res.json({
-      id: id,
-      description: description,
-      duration: duration,
-      date: date,
-    });
+app.get("/api/users", async function (req, res) {
+  try {
+    const collection = database.db("exercise_tracker").collection("users");
+    const users = await collection.find().toArray();
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: `server error: ${err}` });
   }
+});
+
+app.post("/api/users/:_id/exercises", async function (req, res) {
+  const id = req.params._id;
+  const description = req.body.description;
+  const duration = +req.body.duration; //number always
+  const date = req.body.date ? new Date(req.body.date) : new Date();
+
+  const collection = database.db("exercise_tracker").collection("users");
+
+  const user = await collection.findOne({ _id: id });
+
+  //check if user exist
+  if (!user) {
+    return res.status(400).send({ error: "invalid user" });
+  }
+  if (!description || !duration) {
+    return res
+      .status(400)
+      .send({ error: "Description or duration must be provided" });
+  }
+
+  const exercise = {
+    description: description,
+    duration: duration,
+    date: date.toDateString(),
+  };
+  // Update the user document
+  await collection.updateOne(
+    { _id: new ObjectId(id) },
+    { $push: { log: exercise } }
+  );
+  res.json({
+    _id: id,
+    username: user.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date,
+  });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
